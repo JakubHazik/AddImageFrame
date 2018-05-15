628e9675bfe116924956f5180f174c0fd051c26b
Unknown <hazik.jakub@gmail.com>
2018-05-15 22:41:31 +0200

    add icon and add installation

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,15 @@ using namespace Magick;
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+#ifdef __linux__
+    try{
+        QApplication::setWindowIcon(QIcon("/usr/local/bin/icons/icon.png"));
+    } catch (...){
+        std::cout<<"Icon not found"<<endl;
+    }
+#endif
+
     Gui w;
     w.show();
     return QApplication::exec();
