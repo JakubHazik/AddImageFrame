a115ff82ebf6762745549015353974c10ccb764b
Jakub Hazik <hazik.jakub@gmail.com>
2019-05-07 09:27:31 +0200

    finish Win deployment, tidy up repo

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 // Created by jakub on 12.5.2018.
 //
 
+#ifdef WIN32
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")	// hide windows console
+#endif
+
 #include <Magick++.h>
 #include "ImageFrame/ui.h"
 #include <build_defines.h>
@@ -11,14 +15,20 @@ using namespace Magick;
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
+	// choose application icon
 #ifdef __linux__
     try{
         QApplication::setWindowIcon(QIcon(install_dir + "icons/icon.png"));
     } catch (...){
         std::cout<<"Icon not found"<<std::endl;
     }
-#elif __WIN__
-
+#elif WIN32
+	try {
+		QApplication::setWindowIcon(QIcon("icons/icon.png"));
+	}
+	catch (...) {
+		std::cout << "Icon not found" << std::endl;
+	}
 #endif
 
     MainWindow w;
